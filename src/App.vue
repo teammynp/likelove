b55dcffb5f077e132cb869b71ff70309c6789b62
teammynp <68525327+teammynp@users.noreply.github.com>
2020-08-21 20:39:15 +0700

    ++

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,87 @@
 <template>
+<body>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class></li>
+        <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="2" class></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item">
+          <img
+            src="./assets/CD1.jpg"
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            alt
+            srcset
+            aria-label="Placeholder: 500x500"
+          />
+        </div>
+        <div class="carousel-item active">
+          <img
+            src="./assets/CD4.jpg"
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            alt
+            srcset
+            aria-label="Placeholder: 500x500"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="./assets/CD5.jpg"
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+            role="img"
+            alt
+            srcset
+            aria-label="Placeholder: 500x500"
+          />
+        </div>
+      </div>
+    </div>
+    <HelloWorld msg />
+    <div class="row">
+      <div class="col-12">
+        <!-- 2.รับค่าจาก emit @cc แล้วส่งไปทำงานใน function "sum"-->
+        <like label @cc="sum" />
+      </div>
+    </div>
   </div>
+</body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import Like from "./components/Like.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Like,
+  },
+  methods: {
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+    props: {
+      label: String,
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +92,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  background-color:indianred;
 }
 </style>
---
+++ src/components/Like.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="example-1">
+    <!--<button @click="upFunction()" class="btn btn-info">{{label}}</button>-->
+    <button @click="LikeFunction()" class="btn btn-info"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-emoji-smile" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683z"/>
+  <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg></button>
+    <button @click="LoveFunction()" class="btn btn-danger"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+</svg></button>
+    <button @click="HahaFunction()" class="btn btn-warning"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-emoji-laughing" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M12.331 9.5a1 1 0 0 1 0 1A4.998 4.998 0 0 1 8 13a4.998 4.998 0 0 1-4.33-2.5A1 1 0 0 1 4.535 9h6.93a1 1 0 0 1 .866.5z"/>
+  <path d="M7 6.5c0 .828-.448 0-1 0s-1 .828-1 0S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 0-1 0s-1 .828-1 0S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg></button>
+    <button @click="WowFunction()" class="btn btn-success"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-emoji-dizzy" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M9.146 5.146a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zm-5 0a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 1 1 .708.708l-.647.646.647.646a.5.5 0 1 1-.708.708L5.5 7.207l-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708z"/>
+  <path d="M10 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+</svg></button>
+    <button @click="SadFunction()" class="btn btn-info"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-emoji-smile-upside-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 1a7 7 0 1 1 0 14A7 7 0 0 1 8 1zm0-1a8 8 0 1 1 0 16A8 8 0 0 1 8 0z"/>
+  <path fill-rule="evenodd" d="M4.285 6.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 4.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 3.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683z"/>
+  <path d="M7 9.5C7 8.672 6.552 8 6 8s-1 .672-1 1.5.448 1.5 1 1.5 1-.672 1-1.5zm4 0c0-.828-.448-1.5-1-1.5s-1 .672-1 1.5.448 1.5 1 1.5 1-.672 1-1.5z"/>
+</svg></button>
+    <button @click="AngryFunction()" class="btn btn-danger"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-emoji-angry" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683z"/>
+  <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+  <path fill-rule="evenodd" d="M4.053 4.276a.5.5 0 0 1 .67-.223l2 1a.5.5 0 1 1-.447.894l-2-1a.5.5 0 0 1-.223-.67zm7.894 0a.5.5 0 0 0-.67-.223l-2 1a.5.5 0 1 0 .447.894l2-1a.5.5 0 0 0 .223-.67z"/>
+</svg></button>
+    <p v-bind:style="{fontSize: fsize + 'px' }"> {{ counter }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+      fsize:50,
+    }
+  },
+  methods:{
+    LikeFunction: function(){
+      this.counter += 1
+      //1.ส่งค่าตัวแปร counter ออกไปโดยผ่านตัวแปร cc
+      this.$emit('cc',this.counter)
+
+    },
+    LoveFunction: function(){
+      this.counter += 10
+      this.$emit("cc", this.counter);
+    },
+    HahaFunction: function(){
+      this.counter += 5
+      this.$emit("cc", this.counter);
+    },
+    WowFunction: function(){
+      this.counter += 20
+      this.$emit("cc", this.counter);
+    },
+    SadFunction: function(){
+      this.counter -= 5
+      this.$emit("cc", this.counter);
+    },
+    AngryFunction: function(){
+      this.counter -= 50
+      this.$emit("cc", this.counter);
+    },
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+    sum(counterValue){
+      this.total += counterValue
+    }
+  },
+  props: {
+    label: String
+  },
+};
+</script>
+
+<style>
+</style>
+
